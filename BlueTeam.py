03f23c4c30aea6a38049c66df36aff9b59e9fc68
jonzzki16 <50161050+jonzzki16@users.noreply.github.com>
2021-01-09 19:52:14 +0800

    Update BlueTeam.py

--- BlueTeam.py
+++ BlueTeam.py
@@ -1,21 +1,21 @@
 # Blue team, Andrew Nazreth P. Conti, Jonald J. Anda (CPE). Marcus Kyle Realin (ECE)
-
-
 print()
 print('Contact Book Commands')
-print('- Enter "display" - to display contact information.')
+print('- Enter "display" - to display a contact information.')
 print('- Enter "add" - to add new contact information.')
 print('- Enter "change" - to change the the contact information of an existing one.')
 print('- Enter "delete" - to delete a contact information.')
-print('- Enter "contacts" - to display the content of the contact book.')
+print('- Enter "contacts" - to display the contents of the contact book.')
 print('- Enter "end" - to exit the program.')
 print()
+print('For the newly added number just put "0" at the start e.g. (0)9279141839')
+print()
 
-Contact_Book_Number = {'Mr. Andrew Conti': '0927 914 1839'}  # <- EXAMPLE ONLY
-Contact_Book_Email = {'Mr. Andrew Conti': 'anconti@donbosco.edu.ph'}  # <- EXAMPLE ONLY
+Contact_Book_Number = {'Andrew Conti': '09279141839'}  # <- EXAMPLE ONLY
+Contact_Book_Email = {'Andrew Conti': 'anconti@donbosco.edu.ph'} # <- EXAMPLE ONLY
 
 
-def main():
+def contact():
     while True:
         command = str(input("Enter a Command from the Contact Book Commands:"))
         if command == "display":
@@ -28,7 +28,7 @@ def main():
             print("Add a new contact info with the name of the person")
             a = str(input("name: "))
             c = int(input("number: "))
-            e = str(input("email: "))
+            e = str(input("gmail/email: "))
             b = {a: c}
             d = {a: e}
             Contact_Book_Number.update(b)
@@ -47,7 +47,7 @@ def main():
             elif x == 'email':
                 c = str(input("Change Contact Information of a personnel:"))
                 if c in Contact_Book_Email:
-                    Contact_Book_Email[c] = int(input("New Contact Email:"))
+                    Contact_Book_Email[c] = str(input("New Contact gmail/Email:"))
                     print(f"New contact information of {c} is: {Contact_Book_Email[c]}")
                 else:
                     print("Name not found! Please try again or enter another command.")
@@ -65,14 +65,15 @@ def main():
             print(Contact_Book_Email)
             print(Contact_Book_Number)
         elif command == "end":
-            print("Thank You for choosing DBTC! We always do what is best for our Bosconian family.")
-            break
+            print("Thank You for choosing DBTC! We always do what is best for our bosconian family.")
+            exit()
+
         else:
             print('Please enter the correct command.')
 
 
 while True:
     try:
-        main()
+        contact()
     except ValueError:
-        print("Input must be an integer please try again")
+        print("Input must be an integer please try again. Also don't use space.")
